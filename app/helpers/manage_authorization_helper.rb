656f7b131640c310908b6abeba3a39c0a714f26a
Hannah Henderson <hannah.henderson@clio.com>
2024-05-06 15:11:37 -0600

    Provide request body for Manage token HTTP request

--- app/helpers/manage_authorization_helper.rb
+++ app/helpers/manage_authorization_helper.rb
@@ -11,15 +11,18 @@ module ManageAuthorizationHelper
     ENV["CLIO_MANAGE_SITE_URL"] + "oauth/authorize?" + params.to_query
   end
 
-  def manage_token_url(code)
-    params = {
+  def manage_token_url
+    ENV["CLIO_MANAGE_SITE_URL"] + "oauth/token?"
+  end
+
+  def manage_token_body(code)
+    {
       client_id: ENV["CLIO_MANAGE_CLIENT_ID"],
       client_secret: ENV["CLIO_MANAGE_CLIENT_SECRET"],
       grant_type: "authorization_code",
       code: code,
       redirect_uri: ENV["ROOT_URL"] + "manage_callback"
     }
-    ENV["CLIO_MANAGE_SITE_URL"] + "oauth/token?" + params.to_query
   end
 
   def manage_state_valid?(manage_state)
