5c030dce37381c69c5ab947e603123677eb22a6c
Drew Goddyn <Drew.Goddyn@clio.com>
2024-06-17 12:00:16 -0700

    add example-third-party-application.clio.dev to config.hosts in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.hosts << "example-third-party-application.clio.dev"
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
